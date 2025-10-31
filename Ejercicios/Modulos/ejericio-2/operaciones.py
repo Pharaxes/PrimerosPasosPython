b3ab7ff4e7bcb2ab76757adb1aa764a4d6621e33
Martin Emanuel Romero <martinromeroutn@gmail.com>
2025-10-31 20:50:49 -0300

    ejercicio 2 de modulos

---
+++ Ejercicios/Modulos/ejericio-2/operaciones.py
@@ -0,0 +1,15 @@
+# Crear un módulo llamado operaciones.py que contenga funciones para:
+# 1- Sumar dos números
+# 2- Restar dos números
+# 3- Multiplicar dos números
+# Luego, en un archivo main.py importar el módulo y utilizar sus funciones para mostrar resultados
+# en consola.
+
+def sumar(a , b):
+    return a + b
+
+def restar(a , b):
+    return a - b
+
+def multiplicar(a , b):
+    return a * b
